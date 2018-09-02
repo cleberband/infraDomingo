0d050ecc832c6615a1ef228009da4a542d8b22ea
Cleber Bandeira <cleberband@gmail.com>
2018-09-02 14:28:08 +0000

    Nova alteracao em DEV

--- canario_negro.sh
+++ canario_negro.sh
@@ -4,3 +4,12 @@ cowsay -f tux $(fortune)
 
 
 ping -c2 192.168.99.20 >- && echo "Com Conexao" || echo "Sem conexao"
+
+echo -n "
+#####################################
+#				    #
+#	Texto Aleatorio		    #
+#				    #
+#				    #
+#####################################
+"

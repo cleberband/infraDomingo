c489dc91fc5be302db44212d8a3b043887d80ce4
Cleber Bandeira <cleberband@gmail.com>
2018-09-02 13:42:29 +0000

    Alteracao no script da Canario

--- canario_negro.sh
+++ canario_negro.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 echo "Script da Canario"
 cowsay -f tux $(fortune)
-sleep 3
+
+
+ping -c2 192.168.99.20 >- && echo "Com Conexao" || echo "Sem conexao"
+ping -c2 192.168.99.99 >- && echo "Com Conexao" || echo "Sem conexao"
